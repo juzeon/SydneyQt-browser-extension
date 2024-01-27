3b2f07b35c3b5929177ddddf1ed00b8bb3d59f5c
juzeon <skyjuzheng@gmail.com>
2024-01-27 16:28:35 +0800

    adjust pop up, add cookie status

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { StorageItem } from 'webext-storage'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
 async function openBingChat() {
   let bingTabID = new StorageItem<number>('bingTabID')
@@ -14,7 +14,34 @@ async function resolveCaptcha() {
   await openBingChat()
 }
 
-let status = ref('Checking...')
+function checkCookieStatus() {
+  status.value = CookieStatus.checking
+  username.value = ''
+  fetch('https://www.bing.com/search?q=Bing+AI&showconv=1').then(async (res) => {
+    let respText = await res.text()
+    let arr = new RegExp('data-clarity-mask="true" title="(.*?)"').exec(respText)
+    if (arr) {
+      status.value = CookieStatus.loggedIn
+      username.value = arr[1]
+    } else {
+      status.value = CookieStatus.error
+    }
+  }).catch(err => {
+    status.value = CookieStatus.error
+  })
+}
+
+enum CookieStatus {
+  checking = 'Checking...',
+  loggedIn = 'Logged In',
+  error = 'Error',
+}
+
+let status = ref(CookieStatus.checking)
+let username = ref('')
+onMounted(() => {
+  checkCookieStatus()
+})
 </script>
 
 <template>
@@ -23,13 +50,16 @@ let status = ref('Checking...')
       <p class="text-h6 text-center">SydneyQt Browser Extension</p>
       <div class="d-flex my-2 ml-2 align-center">
         <div class="mr-1">Cookie Status:</div>
-        <div class="text-grey">{{ status }}</div>
-        <div class="ml-2" v-if="status==='Checking...'">
-          <v-progress-circular indeterminate size="15" width="2"></v-progress-circular>
+        <div class="text-grey">{{ status + (status === CookieStatus.loggedIn ? ' (' + username + ')' : '')}}</div>
+        <div class="ml-2">
+          <v-progress-circular
+            v-if="status===CookieStatus.checking" indeterminate size="15" width="2"></v-progress-circular>
+          <v-icon v-if="status===CookieStatus.loggedIn" size="15" color="green">mdi-check</v-icon>
+          <v-icon v-if="status===CookieStatus.error" size="15" color="red">mdi-alert</v-icon>
         </div>
       </div>
       <v-card class="fill-height">
-        <v-list>
+        <v-list density="compact">
           <v-list-item title="Open Bing Chat" @click="openBingChat"></v-list-item>
           <v-list-item title="Resolve CAPTCHA" @click="resolveCaptcha"></v-list-item>
         </v-list>
--- src/popup/index.ts
+++ src/popup/index.ts
@@ -4,11 +4,20 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import Popup from './Popup.vue'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
-import Popup from './Popup.vue'
 
 createApp(Popup).use(vuetify).mount('#app')
